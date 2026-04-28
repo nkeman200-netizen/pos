74245d62c6c23527f04139536c3beaa31b6256e3
Sofyan <nkeman200@gmail.com>
2026-04-28 21:19:50 +0700

    hapus script cdn tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>POS Apotek - Sofya Project</title>
     
-    <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = { darkMode: 'class' }
     </script>
